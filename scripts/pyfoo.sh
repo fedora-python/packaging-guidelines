7d82c3b9943a77aec27e2b9e632548a1eb3f8c4a
Thomas Spura <thomas.spura@gmail.com>
2015-05-22 13:30:12 +0200

    Strip out stuff with python in the name
    This helps to track down the real packages with 'py' in the name.
    (There are still some false positives and the source packages are not
    printed yet)

--- scripts/pyfoo.sh
+++ scripts/pyfoo.sh
@@ -3,4 +3,12 @@
 
 #- Find pyfoo packages that need to be renamed:
 #  Current (incomplete) script:
-dnf --disablerepo=* --enablerepo=rawhide search py | awk '{print $1}' | grep py | sort | grep -v python- | grep -v python3-
+non_py_packages=$(dnf --disablerepo=* --enablerepo=rawhide search py | awk '{print $1}' | grep -v i686 | grep py | sort | grep -v python- | grep -v python3- )
+
+without_python3=$(echo $non_py_packages | sed "s/ /\n/g" | grep -v python3)
+
+
+without_python=$(echo $without_python3 | sed "s/ /\n/g" | grep -v python)
+
+
+echo $without_python | sed "s/ /\n/g"
